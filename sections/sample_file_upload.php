9931530b9c3925233f16e9ec1a670e3cd813769e
Lungu Dumitru <dumitru@paymo.biz>
2015-08-17 11:05:38 +0300

    added CURLOPT_SAFE_UPLOAD to sample file upload script

--- sections/sample_file_upload.php
+++ sections/sample_file_upload.php
@@ -1,27 +1,28 @@
 <?php
 
-$target_url = 'https://app.paymoapp.com/api/files';
-$email = 'johndoe@email.com';
-$password = 'secret';
+$target_url = "https://app.paymoapp.com/api/files";
+$email = "johndoe@email.com";
+$password = "secret";
 
-$file_name_with_full_path = '/path/to/file.jpg';
+$file_name_with_full_path = "/path/to/file.jpg";
 $post = array(
-	'project_id' => '123456',
-	'file'=>'@'.$file_name_with_full_path
+	"project_id" => "123456",
+	"file" => "@" . $file_name_with_full_path
 );
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $target_url);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept:application/json'));
+curl_setopt($ch, CURLOPT_HTTPHEADER, array("Accept:application/json"));
 curl_setopt($ch, CURLOPT_USERPWD, $email . ":" . $password);
 curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_SAFE_UPLOAD, 0);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $result = curl_exec($ch);
 
 if ($result === false) {
-	echo 'Curl error: ' . curl_error($ch) . "\n";
+	echo "Curl error: " . curl_error($ch) . "\n";
 }
 
 curl_close($ch);
-echo($result);
+echo $result;
